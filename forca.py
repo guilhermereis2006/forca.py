8f179743959de33dcc2f4c2148a17d0c16fd8891
Reis <129747622+guilhermereis2006@users.noreply.github.com>
2024-04-18 07:52:11 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,30 +1,41 @@
 def jogar():
-  print("*********************************")
-  print("***Bem vindo ao jogo da Forca!***")
-  print("*********************************")
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
 
-  palavra_secreta = "banana"
-  letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
-  enforcou = False
-  acertou = False
+    enforcou = False
+    acertou = False
+    erros = 0
 
-  print(letras_acertadas)
+    print(letras_acertadas)
 
-  while(not enforcou and not acertou):
+    while(not enforcou and not acertou):
 
-      chute = input("Qual letra? ")
-      chute = chute.strip()
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
 
-      index = 0
-      for letra in palavra_secreta:
-          if(chute.upper() == letra.upper()):
-              letras_acertadas[index] = letra
-          index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
-      print(letras_acertadas)
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
 
-  print("Fim do jogo")
+
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
+    print("Fim do jogo")
 
 if(__name__ == "__main__"):
-  jogar()
+    jogar()
